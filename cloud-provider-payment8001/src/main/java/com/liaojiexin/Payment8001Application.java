7d7a29574169928863382eb7c3c18ebc27a1854c
liaojiexin <570839281@qq.com>
2021-02-03 17:52:21 +0800

    commit

---
+++ cloud-provider-payment8001/src/main/java/com/liaojiexin/Payment8001Application.java
@@ -0,0 +1,18 @@
+package com.liaojiexin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: Payment8001Application
+ * @Description: TODO
+ * @version: 1.0
+ * @author: liaojiexin
+ * @date: 2021/2/3 17:18
+ */
+@SpringBootApplication
+public class Payment8001Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8001Application.class, args);
+    }
+}
