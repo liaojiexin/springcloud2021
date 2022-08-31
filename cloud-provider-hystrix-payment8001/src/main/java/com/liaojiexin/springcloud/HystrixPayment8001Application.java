f93aaecd4502b73b1f0313079b942c4fd6c8a1c8
liaojiexin <570839281@qq.com>
2022-08-31 22:15:14 +0800

    hystrix

--- cloud-provider-hystrix-payment8001/src/main/java/com/liaojiexin/springcloud/HystrixPayment8001Application.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/liaojiexin/springcloud/HystrixPayment8001Application.java
@@ -2,6 +2,7 @@ package com.liaojiexin.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @EnableEurekaClient
 @SpringBootApplication
+@EnableCircuitBreaker   //启用Hystrix激活方法
 public class HystrixPayment8001Application {
     public static void main(String[] args) {
         SpringApplication.run(HystrixPayment8001Application.class, args);
--- cloud-provider-hystrix-payment8001/src/main/java/com/liaojiexin/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/liaojiexin/springcloud/service/impl/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.liaojiexin.springcloud.service.impl;
 
 import com.liaojiexin.springcloud.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -29,14 +31,28 @@ public class PaymentServiceImpl implements PaymentService {
      * @return
      */
     @Override
+    @HystrixCommand(fallbackMethod = "pyamentTimeoutHandler",commandProperties = {
+            //设置超时时间为1000毫秒时，就会调用pyamentTimeoutHandler方法来做服务降级
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1000")
+    })
     public String payment_timeout(Integer id) {
         try {
             TimeUnit.SECONDS.sleep(3);
+            //注意这里也会找出服务降级，尽管这里@HystrixProperty没做修改，
+            // 因为这里底层代码里面有一些异常配置，找不到超时的异常会找别的异常
+            //int i=10/0
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         String result="线程池："+Thread.currentThread().getName()+";接口payment_timeout,id"+id;
         System.out.println(result);
+        return result;
+    }
+
+    //这里payment_timeout方法服务降级后的兜底方法，这里要注意该方法要和调用这个方法的参数相同，
+    // 即payment_outtime有一个(Integer id),pyamentTimeoutHandler也要和他一样
+    public String pyamentTimeoutHandler(Integer id){
+        String result="线程池："+Thread.currentThread().getName()+";8001服务接口调用繁忙，请稍后再试";
         return result;
     }
 }
