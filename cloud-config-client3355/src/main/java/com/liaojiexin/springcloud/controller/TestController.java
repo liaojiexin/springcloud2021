5e4ca18d033200fc02ed4bd2966597ea7a24418d
liaojiexin <570839281@qq.com>
2022-09-06 23:12:17 +0800

    config

--- cloud-config-client3355/src/main/java/com/liaojiexin/springcloud/controller/TestController.java
+++ cloud-config-client3355/src/main/java/com/liaojiexin/springcloud/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.liaojiexin.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 10:35 下午 2022/9/6
  **/
 @RestController
+@RefreshScope   //刷新配置
 public class TestController {
 
     //这里获取配置中心服务端中连接gitee上的配置
