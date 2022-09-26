a7b221fb3dfae5ceca9ff54f2fb80b9d35bf2ca8
liaojiexin <570839281@qq.com>
2022-09-26 22:32:19 +0800

    sentinel+nacos+ribbon

--- cloudalibaba-consumer-nacos-order83/src/main/java/com/liaojiexin/springcloud/controller/OrderNacosController.java
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/liaojiexin/springcloud/controller/OrderNacosController.java
@@ -1,6 +1,10 @@
 package com.liaojiexin.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.liaojiexin.springcloud.entity.CommonResult;
+import com.liaojiexin.springcloud.entity.Payment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,5 +27,17 @@ public class OrderNacosController {
     @GetMapping(value = "/nacos/consumer")
     public String get(){
         return restTemplate.getForObject(PAYMENT_URL+"/nacos/payment",String.class);
+    }
+
+    @GetMapping(value = "/consumer/fallback/{id}")
+    @SentinelResource(value = "fallback")   //没有配置
+    public CommonResult<Payment> paymentSql(@PathVariable Long id){
+        CommonResult<Payment> result=restTemplate.getForObject(PAYMENT_URL+"/paymentSql/"+id,CommonResult.class,id);
+        if (id==4){
+            throw new IllegalArgumentException("非法参数异常");
+        }else if (result.getData()==null){
+            throw new NullPointerException("空指针异常");
+        }
+        return result;
     }
 }
--- cloudalibaba-provider-payment9001/src/main/java/com/liaojiexin/springcloud/controller/PaymentController.java
+++ cloudalibaba-provider-payment9001/src/main/java/com/liaojiexin/springcloud/controller/PaymentController.java
@@ -1,8 +1,13 @@
 package com.liaojiexin.springcloud.controller;
 
+import com.liaojiexin.springcloud.entity.CommonResult;
+import com.liaojiexin.springcloud.entity.Payment;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 /**
  * @ClassName PaymentController
@@ -19,5 +24,21 @@ public class PaymentController {
     @GetMapping(value = "/nacos/payment")
     public String get(){
         return "调用成功，端口号："+port;
+    }
+
+
+    public static HashMap<Long, Payment> hashMap=new HashMap<>();
+
+    static {
+        hashMap.put(1L,new Payment(1L,"1111"));
+        hashMap.put(2L,new Payment(2L,"2222"));
+        hashMap.put(3L,new Payment(3L,"3333"));
+    }
+
+    @GetMapping("/paymentSql/{id}")
+    public CommonResult<Payment> paymentSql(@PathVariable("id") Long id){
+        Payment payment=hashMap.get(id);
+        CommonResult<Payment> result=new CommonResult<>(200,"请求成功，端口号："+port,payment);
+        return result;
     }
 }
